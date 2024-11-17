d0e80ca07085f1c660c643c49ebc310902e2bc68
spark4862 <1323237621@qq.com>
2024-11-17 20:39:28 +0800

    init

---
+++ draft.py
@@ -0,0 +1,12 @@
+# import salabim as sim
+#
+# class Car(sim.Component):
+#     def setup(self):
+#         pass
+#
+#     def process(self):
+#         pass
+#
+# with open("./log.log", "rw") as log:
+#     env = sim.Environment(trace=log)
+#     env.run()
